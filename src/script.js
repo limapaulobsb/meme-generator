5beefd753d0912cd74d9b4c65c573b61a444260e
Paulo Henrique Lima <limapaulobsb@gmail.com>
2022-02-21 20:13:13 -0300

    Add GitHub link and color inputs

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@
 const imageInput = document.getElementById('image-input');
 const topTextInput = document.getElementById('top-text-input');
 const bottomTextInput = document.getElementById('bottom-text-input');
+const colorInputs = document.querySelectorAll('input[type="color"]');
 const canvasElement = document.querySelector('canvas');
 const ctx = canvasElement.getContext('2d');
 
@@ -14,8 +15,8 @@ let imageElement;
 function updateCanvas() {
   const { height, width } = imageElement;
   const fontSize = Math.floor(width / 15);
-  const bottomText = bottomTextInput.value;
   const topText = topTextInput.value;
+  const bottomText = bottomTextInput.value;
   const yOffset = height / 25;
 
   // Add canvas background
@@ -24,22 +25,24 @@ function updateCanvas() {
   ctx.drawImage(imageElement, 0, 0);
 
   // Set text configuration
-  ctx.fillStyle = 'white';
   ctx.font = `${fontSize}px Verdana`;
   ctx.lineJoin = 'round';
   ctx.lineWidth = Math.floor(fontSize / 4);
-  ctx.strokeStyle = 'black';
   ctx.textAlign = 'center';
 
-  // Add bottom text
-  ctx.textBaseline = 'bottom';
-  ctx.strokeText(bottomText, width / 2, height - yOffset);
-  ctx.fillText(bottomText, width / 2, height - yOffset);
-
   // Add top text
+  ctx.fillStyle = colorInputs[1].value;
+  ctx.strokeStyle = colorInputs[0].value;
   ctx.textBaseline = 'top';
   ctx.strokeText(topText, width / 2, yOffset);
   ctx.fillText(topText, width / 2, yOffset);
+
+  // Add bottom text
+  ctx.fillStyle = colorInputs[3].value;
+  ctx.strokeStyle = colorInputs[2].value;
+  ctx.textBaseline = 'bottom';
+  ctx.strokeText(bottomText, width / 2, height - yOffset);
+  ctx.fillText(bottomText, width / 2, height - yOffset);
 }
 
 // =====> End Function Declarations <=====
@@ -54,6 +57,7 @@ window.onload = () => {
 
   bottomTextInput.addEventListener('input', updateCanvas);
   topTextInput.addEventListener('input', updateCanvas);
+  colorInputs.forEach((el) => el.addEventListener('input', updateCanvas));
 };
 
 // =====> End Onload Events and Listeners setup <=====
