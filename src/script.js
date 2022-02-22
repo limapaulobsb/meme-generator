72c0bd2b9c94ba6355aced84eb014e037cf54c54
Paulo Henrique Lima <limapaulobsb@gmail.com>
2022-02-22 11:38:12 -0300

    Add background templates

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
+// Project also inspired by this great video on dcode YouTube channel
+// https://www.youtube.com/watch?v=io5FcMAdLyQ
+
 // =====> Start Global Variables <=====
 
 const imageInput = document.getElementById('image-input');
 const topTextInput = document.getElementById('top-text-input');
 const bottomTextInput = document.getElementById('bottom-text-input');
+const exampleImages = document.querySelectorAll('.example-image');
 const colorInputs = document.querySelectorAll('input[type="color"]');
 const canvasElement = document.querySelector('canvas');
 const ctx = canvasElement.getContext('2d');
@@ -31,18 +35,24 @@ function updateCanvas() {
   ctx.textAlign = 'center';
 
   // Add top text
-  ctx.fillStyle = colorInputs[1].value;
-  ctx.strokeStyle = colorInputs[0].value;
+  ctx.fillStyle = colorInputs[0].value;
+  ctx.strokeStyle = colorInputs[1].value;
   ctx.textBaseline = 'top';
   ctx.strokeText(topText, width / 2, yOffset);
   ctx.fillText(topText, width / 2, yOffset);
 
   // Add bottom text
-  ctx.fillStyle = colorInputs[3].value;
-  ctx.strokeStyle = colorInputs[2].value;
+  ctx.fillStyle = colorInputs[2].value;
+  ctx.strokeStyle = colorInputs[3].value;
   ctx.textBaseline = 'bottom';
   ctx.strokeText(bottomText, width / 2, height - yOffset);
   ctx.fillText(bottomText, width / 2, height - yOffset);
+}
+
+function loadImage(source) {
+  imageElement = document.createElement('img');
+  imageElement.src = source;
+  imageElement.addEventListener('load', updateCanvas, { once: true });
 }
 
 // =====> End Function Declarations <=====
@@ -50,13 +60,20 @@ function updateCanvas() {
 
 window.onload = () => {
   imageInput.addEventListener('change', () => {
-    imageElement = document.createElement('img');
-    imageElement.src = URL.createObjectURL(imageInput.files[0]);
-    imageElement.addEventListener('load', updateCanvas, { once: true });
+    const source = URL.createObjectURL(imageInput.files[0]);
+    loadImage(source);
   });
 
-  bottomTextInput.addEventListener('input', updateCanvas);
   topTextInput.addEventListener('input', updateCanvas);
+  bottomTextInput.addEventListener('input', updateCanvas);
+
+  exampleImages.forEach((el) => {
+    el.addEventListener('click', () => loadImage(el.src));
+    el.addEventListener('keyup', ({ key }) => {
+      if (key === 'Enter') loadImage(el.src);
+    });
+  });
+
   colorInputs.forEach((el) => el.addEventListener('input', updateCanvas));
 };
 
